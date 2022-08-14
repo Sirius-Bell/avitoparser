b6110b52c3143d8e5cd4790cb48017bb7a1069f5
Lonely-Dark <vlad10game@gmail.com>
2022-08-14 17:15:09 +0300

    Set the description

--- exceptions.py
+++ exceptions.py
@@ -1,6 +1,10 @@
 class PageSourceNotConfigured(Exception):
 
     def __init__(self, message: str) -> None:
+        """
+        Thrown an error if page source don't transmitted
+        :param message: message to know
+        """
         self.message = message
 
     def __str__(self) -> str:
